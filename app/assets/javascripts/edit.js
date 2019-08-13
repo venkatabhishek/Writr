e135ac55b5d85fe44540ee207a0caf3ac75f58ba
Abhishek Venkat <abhisaivenkat@gmail.com>
2019-08-13 19:04:27 -0400

    image search

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // setup
 
@@ -15,7 +15,7 @@ $(document).ready(function() {
     $.ajax({
         method: "GET",
         url: `/articles/${id}/content`
-    }).done(function(data) {
+    }).done(function (data) {
 
         var quill = new Quill('#editor', {
             modules: {
@@ -30,10 +30,10 @@ $(document).ready(function() {
         content = new Delta(JSON.parse(data.content).ops)
         quill.setContents(content)
 
-        quill.on('text-change', function() {
+        quill.on('text-change', function () {
             save.html("saving...")
             clearTimeout(timeoutId);
-            timeoutId = setTimeout(function() {
+            timeoutId = setTimeout(function () {
                 var newContent = quill.getContents()
                 var diff = content.diff(newContent)
 
@@ -51,7 +51,7 @@ $(document).ready(function() {
                             }
                         }
 
-                    }).done(function(data) {
+                    }).done(function (data) {
 
                         if (data.status == 1) {
                             save.html("saved")
@@ -60,7 +60,7 @@ $(document).ready(function() {
                         }
 
 
-                    }).fail(function(err) {
+                    }).fail(function (err) {
 
                         save.html("Save failed")
 
@@ -80,21 +80,65 @@ $(document).ready(function() {
 
         // add image from unsplash
 
-        $(".unsplash-image").click(function(){
+        $(".unsplash-image").click(function () {
             $(".image-wrapper").css("display", "flex");
         })
 
-        $(".image-close").click(function(){
+        $(".image-close").click(function () {
             $(".image-wrapper").hide();
         })
 
-        $("form#unsplash-search").submit(function(e){
+        $('.image-results').masonry({
+            itemSelector: '.image-option',
+            columnWidth: 200,
+            horizontalOrder: true
+        });
+
+
+        function getImages(query, page) {
+            $.ajax({
+                method: "GET",
+                url: "https://api.unsplash.com/search/photos",
+                data: {
+                    client_id: "45fb81ea68f0f569712dfe304925d9e7753afa93f01c4ca90ccf3ffce423652f",
+                    query: query,
+                    per_page: 20,
+                    page: page
+                }
+            }).done(function (data) {
+                data.results.forEach(img => {
+                   var item =  $(`
+                        <div class="image-option">
+                            <img src="${img.urls.thumb}" alt="">
+                        </div>
+                    `)
+
+                    $(".image-results").append(item).masonry('appended', item).masonry();;
+                })
+
+                
+
+
+            }).fail(function (err) {
+                console.log(err)
+            }).always(function () {
+                
+            })
+        }
+
+        $("form#unsplash-search").submit(function (e) {
             e.preventDefault();
 
-            console.log("submitted")
+            var query = $(".image-search-input").val();
+            $(".image-results").empty()
+
+            getImages(query, 1); 
+
         })
 
-    }).fail(function(e) {
+
+
+    }).fail(function (e) {
         console.log(e)
     })
 
